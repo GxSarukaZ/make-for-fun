7a6b9e69a565b054417d8395911a769fa4825f9b
lpkll <lpkll123@gmail.com>
2022-11-05 20:40:17 +0700

    Added Loot Table Drop STRUCTURES

--- src/main/java/net/anodizex/specialore/item/ModItemGroup.java
+++ src/main/java/net/anodizex/specialore/item/ModItemGroup.java
@@ -10,13 +10,6 @@ import net.minecraft.util.Identifier;
 public class ModItemGroup {
     public static final ItemGroup TIMASCUS = FabricItemGroupBuilder.build(new Identifier(SpecialOreMod.MOD_ID,"timascus"),
             () -> new ItemStack(ModItems.TIMASCUS_INGOT));
-
-    public static final ItemGroup CATALYST = FabricItemGroupBuilder.build(new Identifier(SpecialOreMod.MOD_ID,"catalyst"),
-            () -> new ItemStack(ModItems.BE_THE_ONE_CATALYST));
-
     public static final ItemGroup SPECIALBLOCK = FabricItemGroupBuilder.build(new Identifier(SpecialOreMod.MOD_ID,"specialblock"),
             () -> new ItemStack(ModBlocks.TIMASCUS_BLOCK));
-
-    public static final ItemGroup SPECIALOREMOD = FabricItemGroupBuilder.build(new Identifier(SpecialOreMod.MOD_ID,"specialblock"),
-            () -> new ItemStack(ModItems.DRAGONFLUX));
 }
--- src/main/java/net/anodizex/specialore/util/ModLootTableModifiers.java
+++ src/main/java/net/anodizex/specialore/util/ModLootTableModifiers.java
@@ -3,6 +3,7 @@ package net.anodizex.specialore.util;
 import net.anodizex.specialore.item.ModItems;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.MatchToolLootCondition;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
@@ -14,34 +15,22 @@ import net.minecraft.util.Identifier;
 
 public class ModLootTableModifiers {
 
-    private static final Identifier ANCIENT_CITY_STRUCTURE_CHEST_ID
-            =new Identifier("minecraft","chests/ancient_city");
+    private static final Identifier ANCIENT_CITY_STRUCTURE_ID
+            = new Identifier("minecraft","chest/ancient_city_ice_box");
 
-    private static final Identifier END_CITY_STRUCTURE_CHEST_ID
-            =new Identifier("minecraft","chests/end_city_treasure");
+    private static final Identifier ANCIENT_CITY_STRUCTURE_ID_2
+            = new Identifier("minecraft","chest/ancient_city_ice_box");
 
+    private static final Identifier BLACK_SMITH_STRUCTURE_ID
+            =new Identifier("minecraft","chest/village/village_toolsmith");
+
+    private static final Identifier BLACK_SMITH_STRUCTURE_ID_2
+            =new Identifier("minecraft","chest/village/village_weaponsmith");
     private static final Identifier JUNGLE_TEMPLE_STRUCTURE_ID
             =new Identifier("minecraft","chests/jungle_temple_dispenser");
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-
-            if (ANCIENT_CITY_STRUCTURE_CHEST_ID.equals(id)) {
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(1f))
-                        .with(ItemEntry.builder(ModItems.DRAGON_CATALYST))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
-                tableBuilder.pool(poolBuilder.build());
-            }
-            if (END_CITY_STRUCTURE_CHEST_ID.equals(id)) {
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(1f))
-                        .with(ItemEntry.builder(ModItems.DRAGONFLUX))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
-                tableBuilder.pool(poolBuilder.build());
-            }
             if (JUNGLE_TEMPLE_STRUCTURE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
@@ -50,7 +39,50 @@ public class ModLootTableModifiers {
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
                 tableBuilder.pool(poolBuilder.build());
             }
+        });
 
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (ANCIENT_CITY_STRUCTURE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f))
+                        .with(ItemEntry.builder(ModItems.RAW_TIMASCUS_INGOT))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (BLACK_SMITH_STRUCTURE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f))
+                        .with(ItemEntry.builder(ModItems.RAW_TIMASCUS_INGOT))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+            tableBuilder.pool(poolBuilder.build());
+            }
+        });
+
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (BLACK_SMITH_STRUCTURE_ID_2.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f))
+                        .with(ItemEntry.builder(ModItems.RAW_TIMASCUS_INGOT))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (ANCIENT_CITY_STRUCTURE_ID_2.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f))
+                        .with(ItemEntry.builder(ModItems.FLYFIRE_IN_BOTTLE))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
         });
     }
 }
